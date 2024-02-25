2d1a8d2b9410d06f5d526d549c60c6847f3f5fdc
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-25 21:38:35 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
+const errorElement = document.querySelector('.error');
+const catInfoElement = document.querySelector('.cat-info');
+const breedSelect = document.querySelector('.breed-select');
+const loaderElement = document.querySelector('.loader');
+
